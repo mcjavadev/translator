80f6f2675ba48f2f00433387d611af4567352be1
Alex <alexzatstone@gmail.com>
2020-11-25 23:04:44 +0000

    first commit

---
+++ src/main/java/translatorplugin/me/javadev/translatorplugin/Main.java
@@ -0,0 +1,15 @@
+package translatorplugin.me.javadev.translatorplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
